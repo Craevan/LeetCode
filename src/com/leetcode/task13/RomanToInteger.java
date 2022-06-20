b2279c71216354a1e63f241a585641257a6ca895
a.siverskii <a.siverskii@gmail.com>
2022-06-20 21:26:46 +0300

    add task 13 RomanToInteger

---
+++ src/com/leetcode/task13/RomanToInteger.java
@@ -0,0 +1,36 @@
+package com.leetcode.task13;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        RomanToInteger romanToInteger = new RomanToInteger();
+        System.out.println(romanToInteger.romanToInt("MCMXCIV"));
+    }
+    public int romanToInt(String s) {
+        char[] chars = s.toCharArray();
+        int current;
+        int prev = Integer.MAX_VALUE;
+        int num = 0;
+        for (char ch : chars) {
+            current = convert(ch);
+            num += current;
+            if (prev < current) {
+                num = num - (2 * prev);
+            }
+            prev = current;
+        }
+        return num;
+    }
+
+    private int convert(char c) {
+        return switch (Character.toLowerCase(c)) {
+            case ('i') -> 1;
+            case ('v') -> 5;
+            case ('x') -> 10;
+            case ('l') -> 50;
+            case ('c') -> 100;
+            case ('d') -> 500;
+            case ('m') -> 1000;
+            default -> 0;
+        };
+    }
+}
